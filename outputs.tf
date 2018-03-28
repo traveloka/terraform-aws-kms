1996be6bf35709fb7777c9b96ebd02eb2624be9c
andy <andy@traveloka.com>
2018-03-28 13:37:05 +0700

    Output key alias name

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "key_alias_arn" {
   value       = "${aws_kms_alias.key_alias.arn}"
 }
 
+output "key_alias_name" {
+  description = "The name of the key alias"
+  value       = "${aws_kms_alias.key_alias.name}"
+}
+
 output "key_arn" {
   description = "The arn of the key"
   value       = "${aws_kms_key.key.arn}"
